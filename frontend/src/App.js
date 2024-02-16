4f87ee3b43d69d998a7e4ee389132582ad0c59bc
gr <wkdwlgjs111@naver.com>
2024-02-17 01:07:17 +0900

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+//import {Component} from './components/component/component';
+import Component from './components/component/component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    	<Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter 임포트
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> {/* BrowserRouter로 App 컴포넌트를 감싸기 */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
