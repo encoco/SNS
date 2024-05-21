f2a21ff7f7d35732d900eb6030ffb744981c2916
lsy <seungye1209@gmail.com>
2024-05-21 23:44:55 +0900

    comment

--- frontend/src/components/component/ui/Comment.js
+++ frontend/src/components/component/ui/Comment.js
@@ -1,181 +1,197 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from "../../../api";
 
-function Comment({ isOpen, onClose, comments, boardId }) {
-	const [commentText, setCommentText] = useState('');
-	const [commentText2, setCommentText2] = useState('');
-	const [activeDropdown, setActiveDropdown] = useState(null);
-	const [editCommentId, setEditCommentId] = useState(null);
-	const [originalCommentText, setOriginalCommentText] = useState('');
+function Comment({ isOpen, onClose, boardId }) {
+    const [commentText, setCommentText] = useState('');
+    const [commentText2, setCommentText2] = useState('');
+    const [activeDropdown, setActiveDropdown] = useState(null);
+    const [editCommentId, setEditCommentId] = useState(null);
+    const [originalCommentText, setOriginalCommentText] = useState('');
+    const [comments, setComments] = useState([]);
 
-	if (!isOpen) {
-		return null;
-	}
+    useEffect(() => {
+        if (isOpen) {
+            fetchComments();
+        }
+    }, [isOpen]);
 
+    const fetchComments = async () => {
+        try {
+            const response = await api.get(`/getComments`, {
+                params: { boardId },
+                withCredentials: true,
+            });
+            setComments(response.data);
+        } catch (error) {
+            console.error('Error fetching comments:', error);
+        }
+    };
 
-	const handleClose = () => {
-		onClose();
-		setCommentText('');
-	};
+    const handleClose = () => {
+        onClose();
+        setCommentText('');
+    };
 
-	const handleOverlayClick = () => {
-		onClose();
-	};
+    const handleOverlayClick = () => {
+        onClose();
+    };
 
-	const handleModalContentClick = (e) => {
-		e.stopPropagation();
-	};
+    const handleModalContentClick = (e) => {
+        e.stopPropagation();
+    };
 
-	const handleCommentWrite = async () => {
-		if (isOpen) {
-			console.log("현재 선택된 게시물 ID:", boardId);
-			const requestData = {
-				comment: commentText,
-				board_id: boardId
-			};
+    const handleCommentWrite = async () => {
+        if (isOpen) {
+            const requestData = {
+                comment: commentText,
+                board_id: boardId
+            };
 
-			try {
-				const response = await api.post(`/CommentWrite`, requestData, {
-					withCredentials: true,
-				});
-				if (response.data === "success") {
-					alert('댓글 작성 성공');
-					setCommentText('');
-				} else {
-					alert('댓글 작성 실패');
-				}
-			} catch (error) {
-				console.log(error);
-				alert('댓글 작성 중 오류가 발생했습니다.');
-			}
-		}
-	};
+            try {
+                const response = await api.post(`/CommentWrite`, requestData, {
+                    withCredentials: true,
+                });
+                if (response.data === "success") {
+                    alert('댓글 작성 성공');
+                    setCommentText('');
+                    fetchComments();  // 댓글 작성 후 댓글 목록 새로고침
+                } else {
+                    alert('댓글 작성 실패');
+                }
+            } catch (error) {
+                console.log(error);
+                alert('댓글 작성 중 오류가 발생했습니다.');
+            }
+        }
+    };
 
-	const toggleDropdown = (commentId) => {
-		setActiveDropdown(activeDropdown === commentId ? null : commentId);
-	};
+    const toggleDropdown = (commentId) => {
+        setActiveDropdown(activeDropdown === commentId ? null : commentId);
+    };
 
-	const handleEdit = (commentId, commentText2) => {
-		setEditCommentId(commentId);
-		setOriginalCommentText(commentText2);
-		setCommentText2(commentText2); // 수정할 댓글 내용을 input에 표시
-		setActiveDropdown(null);
-	};
+    const handleEdit = (commentId, commentText2) => {
+        setEditCommentId(commentId);
+        setOriginalCommentText(commentText2);
+        setCommentText2(commentText2); // 수정할 댓글 내용을 input에 표시
+        setActiveDropdown(null);
+    };
 
-	const handleEditConfirm = async (comment) => {
-		try {
-			comment.comment = commentText2;
+    const handleEditConfirm = async (comment) => {
+        try {
+            comment.comment = commentText2;
 
-			const response = await api.post(`/EditComment`, comment, {
-				withCredentials: true,
-			});
+            const response = await api.post(`/EditComment`, comment, {
+                withCredentials: true,
+            });
 
-			if (response.data === "success") {
-				alert('댓글 수정 성공');
-				setEditCommentId(null);
-			} else {
-				alert('댓글 수정 실패');
-			}
-		} catch (error) {
-			console.log(error);
-			alert('댓글 수정 중 오류가 발생했습니다.');
-		}
-	};
+            if (response.data === "success") {
+                alert('댓글 수정 성공');
+                setEditCommentId(null);
+                fetchComments();  // 댓글 수정 후 댓글 목록 새로고침
+            } else {
+                alert('댓글 수정 실패');
+            }
+        } catch (error) {
+            console.log(error);
+            alert('댓글 수정 중 오류가 발생했습니다.');
+        }
+    };
 
-	const handleEditCancel = (commentId) => {
-		console.log(`Cancel edit for comment with ID: ${commentId}`);
-		setEditCommentId(null);
-		setCommentText2(originalCommentText); // 이전에 저장한 댓글 내용으로 복구
-	};
+    const handleEditCancel = (commentId) => {
+        setEditCommentId(null);
+        setCommentText2(originalCommentText); // 이전에 저장한 댓글 내용으로 복구
+    };
 
-	const handleDelete = async (comment) => {
-		try {
-			console.log(comment);
-			
-			const response = await api.post(`/DeleteComment`, comment, {
-				withCredentials: true,
-			});
-			if (response.data === "success") {
-				alert('댓글 삭제 성공');
-			} else {
-				alert('댓글 삭제 실패');
-			}
-		} catch (error) {
-			console.log(error);
-			alert('댓글 삭제 중 오류가 발생했습니다.');
-		}
-		setActiveDropdown(null);
-	};
+    const handleDelete = async (comment) => {
+        try {
+            const response = await api.post(`/DeleteComment`, comment, {
+                withCredentials: true,
+            });
+            if (response.data === "success") {
+                alert('댓글 삭제 성공');
+                fetchComments();  // 댓글 삭제 후 댓글 목록 새로고침
+            } else {
+                alert('댓글 삭제 실패');
+            }
+        } catch (error) {
+            console.log(error);
+            alert('댓글 삭제 중 오류가 발생했습니다.');
+        }
+        setActiveDropdown(null);
+    };
 
-	return (
-		<div className="fixed inset-0  bg-opacity-50 flex justify-center items-center z-50" onClick={handleOverlayClick}>
-			<div className=" bg-white p-6 rounded-lg shadow-lg  max-w-3xl w-full m-4 z-50 relative overflow-hidden" onClick={handleModalContentClick}>
-				<h2 className="text-xl font-bold mb-4">댓글</h2>
-				<div className="mb-4 flex">
-					<input
-						type="text"
-						placeholder="댓글 추가..."
-						className="border border-gray-300 p-2 flex-grow mr-2"
-						value={commentText}
-						onChange={(e) => setCommentText(e.target.value)}
-					/>
-					<button onClick={handleCommentWrite} className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-						댓글 달기
-					</button>
-				</div>
-				<div className="overflow-y-auto max-h-96">
-					<ul>
-						{comments && Array.isArray(comments) && comments.map(comment => (
-							<li key={comment.comment_id} className="mt-1 flex items-center relative">
-								<img src={comment.img} alt="Profile" className="w-10 h-10 rounded-full mr-3" />
-								<div className="flex flex-col flex-grow">
-									{editCommentId === comment.comment_id ? (
-										<input
-											type="text"
-											value={commentText2}
-											onChange={(e) => setCommentText2(e.target.value)}
-											className="border border-gray-300 p-2 flex-grow mr-2"
-										/>
-									) : (
-										<>
-											<strong>{comment.nickname}:</strong>
-											<span className="text-gray-600">{comment.comment}</span>
-										</>
-									)}
-								</div>
-								{editCommentId === comment.comment_id ? (
-									<>
-										<button onClick={() => handleEditConfirm(comment)} className="ml-2 text-gray-500">
-											확인
-										</button>
-										<button onClick={() => handleEditCancel(comment.comment_id)} className="ml-2 text-gray-500">
-											취소
-										</button>
-									</>
-								) : (
-									<button onClick={() => toggleDropdown(comment.comment_id)} className="mr-4 text-gray-500">
-										⋮
-									</button>
-								)}
-								<div onClick={() => setActiveDropdown(null)}>
-									{activeDropdown === comment.comment_id && (
-										<div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow-lg z-10">
-											<ul>
-												<li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleEdit(comment.comment_id, comment.comment)}>수정</li>
-												<li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleDelete(comment)}>삭제</li>
-											</ul>
-										</div>
-									)}
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
-				<button onClick={handleClose} className="absolute top-2 right-2 text-2xl font-bold text-gray-800">&times;</button>
+    if (!isOpen) {
+        return null;
+    }
 
-			</div>
-		</div>
-	);
+    return (
+        <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50" onClick={handleOverlayClick}>
+            <div className="bg-white p-6 rounded-lg shadow-lg max-w-3xl w-full m-4 z-50 relative overflow-hidden" onClick={handleModalContentClick}>
+                <h2 className="text-xl font-bold mb-4">댓글</h2>
+                <div className="mb-4 flex">
+                    <input
+                        type="text"
+                        placeholder="댓글 추가..."
+                        className="border border-gray-300 p-2 flex-grow mr-2"
+                        value={commentText}
+                        onChange={(e) => setCommentText(e.target.value)}
+                    />
+                    <button onClick={handleCommentWrite} className="bg-black hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        댓글 달기
+                    </button>
+                </div>
+                <div className="overflow-y-auto max-h-96">
+                    <ul>
+                        {comments && Array.isArray(comments) && comments.map(comment => (
+                            <li key={comment.comment_id} className="mt-1 flex items-center relative">
+                                <img src={comment.img} alt="Profile" className="w-10 h-10 rounded-full mr-3" />
+                                <div className="flex flex-col flex-grow">
+                                    {editCommentId === comment.comment_id ? (
+                                        <input
+                                            type="text"
+                                            value={commentText2}
+                                            onChange={(e) => setCommentText2(e.target.value)}
+                                            className="border border-gray-300 p-2 flex-grow mr-2"
+                                        />
+                                    ) : (
+                                        <>
+                                            <strong>{comment.nickname}:</strong>
+                                            <span className="text-gray-600">{comment.comment}</span>
+                                        </>
+                                    )}
+                                </div>
+                                {editCommentId === comment.comment_id ? (
+                                    <>
+                                        <button onClick={() => handleEditConfirm(comment)} className="ml-2 text-gray-500">
+                                            확인
+                                        </button>
+                                        <button onClick={() => handleEditCancel(comment.comment_id)} className="ml-2 text-gray-500">
+                                            취소
+                                        </button>
+                                    </>
+                                ) : (
+                                    <button onClick={() => toggleDropdown(comment.comment_id)} className="mr-4 text-gray-500">
+                                        ⋮
+                                    </button>
+                                )}
+                                <div onClick={() => setActiveDropdown(null)}>
+                                    {activeDropdown === comment.comment_id && (
+                                        <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow-lg z-10">
+                                            <ul>
+                                                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleEdit(comment.comment_id, comment.comment)}>수정</li>
+                                                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleDelete(comment)}>삭제</li>
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <button onClick={handleClose} className="absolute top-2 right-2 text-2xl font-bold text-gray-800">&times;</button>
+            </div>
+        </div>
+    );
 }
 
 export default Comment;
